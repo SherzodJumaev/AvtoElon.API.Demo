e8f720435357a272cb092462d60bb5c4f4164adc
Sherzod Jumayev <jumayevsherzod304@gmail.com>
2025-02-24 16:44:03 +0500

    add car model

---
+++ Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace AvtoElon.API.Demo.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string CarBody { get; set; } = string.Empty;
+        public string Transmission { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string? Definition { get; set; }
+        public decimal Mileage { get; set; }
+    }
+}
